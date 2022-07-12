370b2f242dbffa5613502a18e5f85ea6cd3169fc
ram-221 <rameshmalli857.com>
2022-07-12 23:30:09 +0530

    UC1

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,12 @@
+package com.bridgelabday04usecases;
+
+public class SnakeLadderUC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int startPosition = 0; 
+	    System.out.println("Game start and Player is at Position " +startPosition);
+	}
+
+}
